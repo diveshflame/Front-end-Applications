892f7c63cc7fac563a5d7733c224fb6bb4fb53ab
divesh <shabdhishetty@gmail.com>
2023-07-11 17:34:50 +0530

    portfolio page

---
+++ New/test.js
@@ -0,0 +1,16 @@
+function walk(){
+console.log("walking....")
+}
+
+function run(){
+console.log("running...")
+}
+
+function move(str){
+// if()
+
+}
+
+const baby = move("walk")
+const adult = move(run)
+// [2,4,6]
